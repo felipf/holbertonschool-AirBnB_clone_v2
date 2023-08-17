3cad0727fdd603a74750fb307d9c32f950a9e7f0
felipf <felipereiraforte@gmail.com>
2023-08-17 11:15:07 -0700

    changing files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application
-
-    python3 -m web_flask.1-hbnb_route
-    curl 0.0.0.0:5000/hbnb ; echo "" | cat -e
-"""
+""" Script that starts a Flask web application """
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def route_index():
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Function that returns Hello HBNB """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def route_HBNB():
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function that returns HBNB """
+    return 'HBNB'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
